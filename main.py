9a9ade398e2a2e4100278299b66b10a07dbdd5be
Alican <alicandonmez90@gmail.com>
2021-10-04 16:11:33 +0200

    add comments

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from requests.models import PreparedRequest
 from config import config
 
 
-def main():
+def main() -> str:
+    """
+    A command line utility for fetching vendor name based on mac-address
+    :accepts a mac-address parameter. Run `python main.py -h` for help
+    :return: name of the vendor for mac address, or error message
+    """
     parser = argparse.ArgumentParser(description='Fetch vendor name of a mac address.')
     parser.add_argument('-m', '--mac_address', required=True)
     args = parser.parse_args()
@@ -21,11 +26,13 @@ def main():
 
     resp = requests.get(req.url)
     if not resp.ok:
-        print(f'Error while fetching vendor name for the mac address {mac_address}\n'
-              f'=> HTTP Status Code: {resp.status_code}, {resp.text}')
+        result = f'Error while fetching vendor name for the mac address {mac_address}\n=>' \
+                 f' HTTP Status Code: ''{resp.status_code}, {resp.text}'
     else:
-        print(f'Vendor Name for the mac address {mac_address} is {resp.text}')
+        result = f'Vendor Name for the mac address {mac_address} is {resp.text}'
+
+    return result
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
